bc0f53f35d50b61741c8a83ad341e600b63f9280
Immo Struchholz <mrerody@googlemail.com>
2017-03-03 00:57:19 +0100

    add error handling

--- app.js
+++ app.js
@@ -36,6 +36,27 @@ app.get('/:time', (req, res) => {
 	}
 });
 
+// dev error handling
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
 app.listen(port, () => {
 	console.log(`listening on port ${port}...`);
 });
